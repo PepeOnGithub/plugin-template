13d760cbd9211a822e5ea3540d5761f12a95566c
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2024-08-30 21:24:36 +0200

    Hope this works

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,56 +1,85 @@
+import { logger } from "@vendetta";
+import Settings from "./Settings";
 import { registerCommand } from "@vendetta/commands";
 import { findByProps } from "@vendetta/metro";
+import { url } from "@vendetta/metro/common";
+import { ApplicationCommandType, ApplicationCommandInputType, ApplicationCommandOptionType } from "../../../ApplicationCommandTypes";
 
-const MessageActions = findByProps("sendMessage", "receiveMessage");
-const commands = [];
+const { sendMessage, receiveMessage } = findByProps("sendMessage", "receiveMessage");
+const { sendBotMessage } = findByProps("sendBotMessage");
+const APIUtils = findByProps("getAPIBaseURL", "get");
+let commands = [];
 const getRandomNumber = () => Math.floor(Math.random() * 100);
 
 const words = [
-"### Get Raided LOL!",
-"### BOZO ASS SERVER!",
-"### I should have brought a condom because this server has no protection",
-"### Look I did the funny",
-"# Hey look || @everyone ||",
-"# Sorry for the ping || @here ||",
-"### This server is getting raided by a plugin LMAO!!!",
-"### Skill Issue"
+    "### Get Raided LOL!",
+    "### BOZO ASS SERVER!",
+    "### I should have brought a condom because this server has no protection",
+    "### Look I did the funny",
+    "# Hey look || @everyone ||",
+    "# Sorry for the ping || @here ||",
+    "### This server is getting raided by a plugin LMAO!!!",
+    "### Skill Issue"
 ];
 
-function randomWord(any: string | any[]) {
-return any[Math.floor(Math.random() * any.length)];
+function randomWord(any) {
+    return any[Math.floor(Math.random() * any.length)];
 }
 
-function sleep(ms: number | undefined) {
-return new Promise((resolve) => setTimeout(resolve, ms));
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-commands.push(
-registerCommand({
-name: "raid1",
-displayName: "raid1",
-description: "raid1",
-displayDescription: "Executes a raid!",
-options: [],
-// @ts-ignore
-applicationId: "-1",
-inputType: 1,
-type: 1,
+const spamCommand = registerCommand({
+    name: "spam",
+    displayName: "spam",
+    description: "Spams any message",
+    displayDescription: "Spams any message",
+    options: [
+        {
+            name: "amount",
+            displayName: "amount",
+            description: "Enter the number of times to send the message.",
+            displayDescription: "Enter the number of times to send the message.",
+            required: true,
+            type: ApplicationCommandOptionType.INTEGER,
+        },
+        {
+            name: "sleep",
+            displayName: "sleep",
+            description: "Enter the time delay between each message in milliseconds.",
+            displayDescription: "Enter the time delay between each message in milliseconds.",
+            required: true,
+            type: ApplicationCommandOptionType.INTEGER,
+        }
+    ],
+    applicationId: "-1",
+    inputType: ApplicationCommandInputType.BOT,
+    type: ApplicationCommandType.CHAT_INPUT,
 
-execute: async (args, ctx) => {
-  for (let idx = 0; idx < 5; idx++) {
-    const rn = getRandomNumber();
-    const rw = randomWord(words);
-    const sym = "`"; // SYM is short for symbol
-    const content = `${rw} ${sym} ${rn} ${sym}`;
-    await sleep(idx * 1000);
-    MessageActions.sendMessage(ctx.channel.id, { content });
-  }
-}
-})
-);
+    execute: async (args, ctx) => {
+        const amount = args[0].value;
+        const sleepTime = args[1].value;
 
-export const onUnload = () => {
-for (const unregisterCommand of commands) {
-unregisterCommand();
+        for (let idx = 0; idx < amount; idx++) {
+            const rw = randomWord(words);
+            const rn = getRandomNumber();
+            const sym = "`";
+            const content = `${rw} ${sym} ${rn} ${sym}`;
+            await sleep(sleepTime);
+            sendMessage(ctx.channel.id, { content });
+        }
+    },
+});
+
+export default {
+    onLoad: () => {
+        logger.log("Hello world!");
+        commands.push(spamCommand);
+    },
+    onUnload: () => {
+        logger.log("Goodbye, world.");
+        commands.splice(commands.indexOf(spamCommand), 1);
+    },
+    settings: Settings,
 }
-};
