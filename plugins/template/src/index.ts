14d07ccda2520b7c2d00eab7696d259dc42be3e0
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2024-08-30 22:20:14 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,14 +1,14 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
-import { registerCommand } from "@vendetta/commands";
+import { registerCommand, unregisterCommand } from "@vendetta/commands";
 import { findByProps } from "@vendetta/metro";
-import { url } from "@vendetta/metro/common";
 import { ApplicationCommandType, ApplicationCommandInputType, ApplicationCommandOptionType } from "../../../ApplicationCommandTypes";
+import { getUser } from "@vendetta/api/users";
 
-const { sendMessage, receiveMessage } = findByProps("sendMessage", "receiveMessage");
-const { sendBotMessage } = findByProps("sendBotMessage");
+const { sendMessage } = findByProps("sendMessage", "receiveMessage");
 const APIUtils = findByProps("getAPIBaseURL", "get");
 let commands = [];
+
 const getRandomNumber = () => Math.floor(Math.random() * 100);
 
 const words = [
@@ -56,7 +56,6 @@ const spamCommand = registerCommand({
     applicationId: "-1",
     inputType: ApplicationCommandInputType.BOT,
     type: ApplicationCommandType.CHAT_INPUT,
-
     execute: async (args, ctx) => {
         const amount = args[0].value;
         const sleepTime = args[1].value;
@@ -72,14 +71,49 @@ const spamCommand = registerCommand({
     },
 });
 
+const avatarCommand = registerCommand({
+    name: "avatar",
+    displayName: "avatar",
+    description: "Fetch and display a user's avatar.",
+    displayDescription: "Fetch and display a user's avatar.",
+    options: [
+        {
+            name: "user_id",
+            displayName: "user_id",
+            description: "The ID of the user to fetch the avatar for.",
+            displayDescription: "The ID of the user to fetch the avatar for.",
+            required: true,
+            type: ApplicationCommandOptionType.STRING,
+        },
+    ],
+    applicationId: "-1",
+    inputType: ApplicationCommandInputType.BOT,
+    type: ApplicationCommandType.CHAT_INPUT,
+    execute: async (args, ctx) => {
+        const userId = args[0].value;
+        try {
+            const user = await getUser(userId);
+            if (!user) {
+                logger.log(`User with ID ${userId} not found.`);
+                return;
+            }
+            const avatarUrl = `${APIUtils.getAPIBaseURL()}/users/${user.id}/avatars/${user.avatar}.png`;
+            sendMessage(ctx.channel.id, { content: `Avatar URL for ${user.username}: ${avatarUrl}` });
+        } catch (error) {
+            logger.error("Error fetching avatar:", error);
+        }
+    },
+});
+
 export default {
     onLoad: () => {
         logger.log("Hello world!");
-        commands.push(spamCommand);
+        commands.push(spamCommand, avatarCommand);
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
-        commands.splice(commands.indexOf(spamCommand), 1);
+        commands.forEach(command => unregisterCommand(command.name));
+        commands = [];
     },
     settings: Settings,
-}
+};
